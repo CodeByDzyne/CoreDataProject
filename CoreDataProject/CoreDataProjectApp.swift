bd22461c82e80ac95c9b83a241d13e52f589d078
CodeByDzyne <wdenley@dzynesolutions.com>
2022-12-28 16:39:59 +1100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Wealthy Waz on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
